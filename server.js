141ccff4d17ee351c2ff67fbce5283cfabdbfa13
nish_noob <agnish13@gmail.com>
2020-11-30 23:57:29 +0400

    Added a express server to serve the static build pages

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.listen(process.env.PORT || 3000);
